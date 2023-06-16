5a2822ee31562212008eed8c48760899f1a0178e
Netsune <jbancroft190@gmail.com>
2023-06-16 20:10:12 +1200

    start of buffer fuzzing setup

--- OverBuff.py
+++ OverBuff.py
@@ -1,1 +1,22 @@
 "#!/usr/bin/python3" 
+
+import sys, socket as so, struct, time
+
+#setting the beginning buffer values and increment steps
+buffer = "A"
+max_buffer = 10000
+increment = 200
+counter = 100
+
+ip = '192.168.0.177'
+port = 9999
+
+s = so.socket(so.AF_INET, so.SOCK_STREAM)
+
+while True:
+    try:
+        print("[+] Fuzzing with %s bytes" % len(buffer))
+        s.connect((ip, port))
+    except:
+        print("[!] connection refused, check debugger")
+        sys(exit)
